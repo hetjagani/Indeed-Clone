f8aa9e6315cc63a25934a98619e65b9759a91420
hetjagani <hetpatel572@gmail.com>
2021-11-02 15:28:56 -0700

    updated mongo init script

--- db-init-scripts/init.js
+++ db-init-scripts/init.js
@@ -2,32 +2,32 @@ print('Start #################################################################')
 
 db = db.getSiblingDB('company');
 db.createUser({
-  user: 'uberadmin',
-  pwd: 'uberadminpass',
+  user: 'indeedadmin',
+  pwd: 'indeedadminpass',
   roles: [{ role: 'readWrite', db: 'auth' }],
 });
 db.createCollection('companies');
 
 db = db.getSiblingDB('user');
 db.createUser({
-  user: 'uberadmin',
-  pwd: 'uberadminpass',
+  user: 'indeedadmin',
+  pwd: 'indeedadminpass',
   roles: [{ role: 'readWrite', db: 'restaurant' }],
 });
 db.createCollection('users');
 
 db = db.getSiblingDB('application');
 db.createUser({
-  user: 'uberadmin',
-  pwd: 'uberadminpass',
+  user: 'indeedadmin',
+  pwd: 'indeedadminpass',
   roles: [{ role: 'readWrite', db: 'customer' }],
 });
 db.createCollection('applications');
 
 db = db.getSiblingDB('review');
 db.createUser({
-  user: 'uberadmin',
-  pwd: 'uberadminpass',
+  user: 'indeedadmin',
+  pwd: 'indeedadminpass',
   roles: [{ role: 'readWrite', db: 'order' }],
 });
 db.createCollection('reviews');
