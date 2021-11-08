c08d59648c512a90959012ab9a39bdf495919e6a
AkashRupapara <akash.rupapara@gmail.com>
2021-11-07 20:23:29 -0500

    Added Model For User

---
+++ user/model.js
@@ -0,0 +1,20 @@
+const { Schema, Types, model } = require('mongoose');
+
+const UserSchema = new Schema({
+  name: String,
+  contactNo: String,
+  emails: [String],
+  resumes: [String],
+  coverLetters: [String],
+  city: String,
+  state: String,
+  country: String,
+  zip: String,
+  jobPreferences: [String]
+});
+
+const User = model('users', UserSchema);
+
+module.exports = {
+  User,
+};
