bf742af48bd08064e0ca195132265dc5dca93e6c
Jainam Patel <jainamwork01@gmail.com>
2021-11-21 02:19:07 -0800

    Review Service Boilerplate

---
+++ review/routes/review.route.js
@@ -0,0 +1,107 @@
+/* eslint-disable newline-per-chained-call */
+const express = require('express');
+const { body } = require('express-validator');
+const {
+  getAllReviews,
+  createReview,
+  getReviewById,
+  updateReview,
+  deleteReview,
+} = require('../controllers/review');
+
+const employerCheckMiddleware = require('../util/employerCheck');
+
+const jobsRouter = require('./job.route');
+
+const router = express.Router();
+
+router.use('/:compId/jobs', employerCheckMiddleware, jobsRouter);
+
+/**
+ * @typedef Company
+ * @property {string} name.required
+ * @property {object} description.required
+ * @property {string} about.required
+ * @property {string} workCulture.required
+ * @property {string} values.required
+ * @property {string} mission.required
+ * @property {string} foundedOn.required
+ * @property {string} ceo.required
+ * @property {object} industry.required
+ * @property {string} headquarters.required
+ * @property {string} revenue.required
+ * @property {integer} size.required
+ * @property {string} website.required
+ * @property {object} media
+ */
+
+const bodyValidators = () => [
+  body('name').exists().isString(),
+  body('description').isObject(),
+  body('about').isString(),
+  body('workCulture').isString(),
+  body('values').isString(),
+  body('mission').isString(),
+  body('foundedOn').exists().isString().isDate({ format: 'mm/dd/yyyy' }),
+  body('ceo').isString(),
+  body('industry').isObject(),
+  body('headquarters').isString(),
+  body('revenue').isString(),
+  body('size').isNumeric(),
+  body('website').isString(),
+  body('media').optional({ nullable: true }).isArray(),
+];
+
+/**
+ * Get list of Companies
+ * @route GET /companies
+ * @param {integer} page.query
+ * @param {integer} limit.query
+ * @group Company
+ * @security JWT
+ * @returns {Array.<Company>} 200 - List of company info
+ */
+router.get('/', getAllCompanies);
+
+/**
+ * Create a Company
+ * @route POST /companies
+ * @group Company
+ * @security JWT
+ * @param {Company.model} Company.body.require
+ * @returns {Company.model} 201 - Created Company
+ */
+router.post('/', ...bodyValidators(), createCompany);
+
+/**
+ * Get Company by ID
+ * @route GET /companies/{id}
+ * @group Company
+ * @security JWT
+ * @param {string} id.path.require
+ * @returns {Company.model} 200 - Company for given ID
+ */
+router.get('/:id', getCompanyById);
+
+/**
+ * Update Company by ID
+ * @route PUT /companies/{id}
+ * @group Company
+ * @security JWT
+ * @param {string} id.path.require
+ * @param {Company.model} Company.body.require
+ * @returns {Company.model} 200 - Updated Company
+ */
+router.put('/:id', ...bodyValidators(), updateCompany);
+
+/**
+ * Delete Company by ID
+ * @route DELETE /companies/{id}
+ * @group Company
+ * @security JWT
+ * @param {string} id.path.require
+ * @returns {null} 200 - Delete Company
+ */
+router.delete('/:id', deleteCompany);
+
+module.exports = router;
