5425289fdd8517e0e8ababf602805043c35b499c
Jainam Patel <jainamwork01@gmail.com>
2021-11-21 15:46:22 -0800

    Updated Kafka-Backend server file

--- kafka-backend/server.js
+++ kafka-backend/server.js
@@ -1,20 +1,19 @@
 require('./config');
 var connection = new require('./kafka/connection');
-// var createUser = require('./services/user/create');
+var createUser = require('./services/user/create');
 
-// const createEmployer = require('./services/employer/create');
-// const updateEmployer = require('./services/employer/update');
-// const deleteEmployer = require('./services/employer/delete');
-// const createCompany = require('./services/company/create');
-// const updateCompany = require('./services/company/update');
-// const deleteCompany = require('./services/company/delete');
-// const createJob = require('./services/job/create');
-// const updateJob = require('./services/job/update');
-// const deleteJob = require('./services/job/delete');
+const createEmployer = require('./services/employer/create');
+const updateEmployer = require('./services/employer/update');
+const deleteEmployer = require('./services/employer/delete');
+const createCompany = require('./services/company/create');
+const updateCompany = require('./services/company/update');
+const deleteCompany = require('./services/company/delete');
+const createJob = require('./services/job/create');
+const updateJob = require('./services/job/update');
+const deleteJob = require('./services/job/delete');
 const createReview = require('./services/review/create');
 const updateReview = require('./services/review/update');
 const deleteReview = require('./services/review/delete');
-
 
 function handleTopicRequest(topic_name, fname) {
   var consumer = connection.getConsumer(topic_name);
@@ -59,16 +58,16 @@ function handleTopicRequest(topic_name, fname) {
 // Add your TOPICs here
 //first argument is topic name
 //second argument is a function that will handle this topic request
-// handleTopicRequest('user.create', createUser);
-// handleTopicRequest('employer.create', createEmployer);
-// handleTopicRequest('employer.update', updateEmployer);
-// handleTopicRequest('employer.delete', deleteEmployer);
-// handleTopicRequest('company.create', createCompany);
-// handleTopicRequest('company.update', updateCompany);
-// handleTopicRequest('company.delete', deleteCompany);
-// handleTopicRequest('job.create', createJob);
-// handleTopicRequest('job.update', updateJob);
-// handleTopicRequest('job.delete', deleteJob);
+handleTopicRequest('user.create', createUser);
+handleTopicRequest('employer.create', createEmployer);
+handleTopicRequest('employer.update', updateEmployer);
+handleTopicRequest('employer.delete', deleteEmployer);
+handleTopicRequest('company.create', createCompany);
+handleTopicRequest('company.update', updateCompany);
+handleTopicRequest('company.delete', deleteCompany);
+handleTopicRequest('job.create', createJob);
+handleTopicRequest('job.update', updateJob);
+handleTopicRequest('job.delete', deleteJob);
 handleTopicRequest('review.create', createReview);
 handleTopicRequest('review.update', updateReview);
 handleTopicRequest('review.delete', deleteReview);
