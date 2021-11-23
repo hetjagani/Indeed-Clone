63fee54fad89ec56afb26feaa822eea76594afd1
Harshil Darji <hd3adpool@pop-os.localdomain>
2021-11-23 01:43:12 -0800

    added searchbars

--- web/src/components/Navbar.jsx
+++ web/src/components/Navbar.jsx
@@ -1,38 +1,42 @@
 import React from 'react';
+
 import Nav from './Nav';
 import NavLeft from './NavLeft';
-// import Beforelogin from './Beforelogin';
-// import Box from '@mui/material/Box';
+import Beforelogin from './Beforelogin';
+import getLoginDetails from '../utils/getLoginDetails';
 
-const Navbar = () => (
-  <div style={{ display: 'flex', flexDirection: 'row' }}>
-    <img
-      src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Indeed_logo.svg/2560px-Indeed_logo.svg.png"
-      style={{
-        width: '120px',
-        height: '30px',
-        marginTop: '30px',
-        marginLeft: '10px',
-      }}
-      alt="indeed"
-    />
-    <div
-      style={{
-        display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
-        width: '100%',
-      }}
-    >
-      <div style={{ marginLeft: '20px', width: '65%' }}>
-        <Nav />
-      </div>
-      <div style={{ width: '35%' }}>
-        {/* <Beforelogin /> */}
-        <NavLeft />
+const Navbar = () => {
+  const decoded = getLoginDetails();
+
+  return (
+    <div style={{ display: 'flex', flexDirection: 'row' }}>
+      <img
+        src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Indeed_logo.svg/2560px-Indeed_logo.svg.png"
+        style={{
+          width: '120px',
+          height: '30px',
+          marginTop: '30px',
+          marginLeft: '10px',
+        }}
+        alt="indeed"
+      />
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+          width: '100%',
+        }}
+      >
+        <div style={{ marginLeft: '20px', width: '90%' }}>
+          <Nav />
+        </div>
+        <div style={{ width: '35%' }}>
+          {decoded ? <NavLeft /> : <Beforelogin />}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 export default Navbar;
