915a199d8095e9ceec79405a2851dfd95e89cc82
hetjagani <hetpatel572@gmail.com>
2021-11-23 18:45:48 -0800

    company: added endpoint for company photos

---
+++ company/controllers/photo.js
@@ -0,0 +1,83 @@
+const { default: axios } = require('axios');
+const { validationResult } = require('express-validator');
+const { errors } = require('u-server-utils');
+
+const getCompanyPhotos = async (req, res) => {
+  try {
+    const { page, limit } = req.query;
+    const { compId } = req.params;
+
+    const photoResp = await axios.get(`${global.gConfig.photos_url}/photos`, {
+      params: { page, limit, companyId: compId },
+      headers: { Authorization: req.headers.authorization },
+    });
+
+    res.status(200).json(photoResp.data);
+  } catch (err) {
+    console.log(err);
+    if (err.isAxiosError) {
+      res.status(err.response.status).json(err.response.data);
+      return;
+    }
+    res.status(500).json(errors.serverError);
+  }
+};
+
+const getCompanyPhotoById = async (req, res) => {
+  try {
+    const { compId, photoId } = req.params;
+
+    const photoResp = await axios.get(`${global.gConfig.photos_url}/photos/${photoId}`, {
+      params: { companyId: compId },
+      headers: { Authorization: req.headers.authorization },
+    });
+
+    res.status(200).json(photoResp.data);
+  } catch (err) {
+    console.log(err);
+    if (err.isAxiosError) {
+      res.status(err.response.status).json(err.response.data);
+      return;
+    }
+    res.status(500).json(errors.serverError);
+  }
+};
+
+const updateCompanyPhoto = async (req, res) => {
+  try {
+    const { compId, photoId } = req.params;
+
+    const valErr = validationResult(req);
+    if (!valErr.isEmpty()) {
+      console.error(valErr);
+      res.status(400).json({ status: 400, message: valErr.array() });
+      return;
+    }
+
+    const { isFeatured } = req.body;
+
+    const response = await axios.put(
+      `${global.gConfig.photos_url}/photos/${photoId}`,
+      { isFeatured },
+      {
+        params: { companyId: compId },
+        headers: { Authorization: req.headers.authorization },
+      },
+    );
+
+    res.status(200).json(response.data);
+  } catch (err) {
+    console.log(err);
+    if (err.isAxiosError) {
+      res.status(err.response.status).json(err.response.data);
+      return;
+    }
+    res.status(500).json(errors.serverError);
+  }
+};
+
+module.exports = {
+  getCompanyPhotoById,
+  getCompanyPhotos,
+  updateCompanyPhoto,
+};
