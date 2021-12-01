6a6667df3719f0b4b214ab609e7a34a8f134bc97
Harshil Darji <hd3adpool@pop-os.localdomain>
2021-11-30 17:08:25 -0800

    minor routing fix

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -28,7 +28,6 @@ function App() {
           <Route path="/cmp/:id" component={withAuth(CompanyMain, 'any', true)} />
           <Route path="/reviews" component={withAuth(FindCompany, 'any', true)} />
           <Route path="/salaries" component={withAuth(FindSalary, 'any', true)} />
-          <Route path="/salary" component={withAuth(Salary, 'any', true)} />
           <Route path="/employee" component={withAuth(Employeedetails, 'any', true)} />
           <Route path="/employee/company" component={withAuth(Companydetails, 'any', true)} />
           <Route path="/employee/companyValues" component={withAuth(CompanyValues, 'any', true)} />
